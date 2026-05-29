5c1126abed75e80c6181d4e591fd8600d7ea8673
lolitaamazza <lolamazzaoliver@gmail.com>
2026-05-28 21:27:47 -0300

    aa

--- src/app/components/ClientFooterControl.jsx
+++ src/app/components/ClientFooterControl.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation"; //hook nativo
 import Footer from "./Footer";
 
 export default function ClientFooterControl() {
+    //usePathname da la URL actual (ej: "/" o "/movie/1339713"). si da null, le pongo un string vacío para que no se rompa
   const pathname = usePathname() || "";
-
-  //oculta el footer en cualquier ruta que comience con /movie
+  //si la URL arranca con "/movie", significa que estoy en la pantalla de detalle. en ese caso, retorno 'null' para que el footer no se renderice y el detalle ocupe toda la pantalla 
   if (pathname.startsWith("/movie")) return null;
 
   return <Footer />;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar"; // 1. IMPORTAMOS EL NAVBAR
+import Navbar from "./components/Navbar"; 
 import Footer from "./components/Footer";
 import ClientFooterControl from "./components/ClientFooterControl";
 
@@ -15,14 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" className="bg-black scroll-smooth scrollbar-hide">
       <body className={`${inter.className} min-h-screen flex flex-col bg-black text-white antialiased scrollbar-hide`}>
-        {/* 2. ACOMODAMOS EL NAVBAR ARRIBA DEL CONTENIDO */}
         <Navbar />
 
         <div className="grow">
           {children}
         </div>
 
-        {/* Footer mostrado condicionalmente desde un componente cliente */}
+        {/*footer mostrado condicionalmente desde un componente cliente*/}
         <ClientFooterControl />
         
       </body>
